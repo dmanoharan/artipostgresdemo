dee8ddda6d0a7d50eef1d5a6a6299fee8b56cf8b
Dhilipan Manoharan <dmanoharan@telecomsys.com>
2018-06-26 15:33:17 -0700

    Signed-off-by: Dhilipan Manoharan <dmanoharan@telecomsys.com>

---
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -0,0 +1,30 @@
+package bootwildfly;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import java.lang.Iterable;
+import bootwildfly.Carrier;
+@RestController
+public class HelloWildFlyController {
+
+        	@Autowired
+	private CarrierRepository carrierRepository;
+        
+@Value("${app.team}")
+        private String appTeam;
+    @RequestMapping("hello")
+    public String sayHello(){
+        return ("Hello, SpringBoot on Wildfly"+appTeam);
+    }
+        
+        @RequestMapping("carriers")
+    public Iterable<Carrier> sayHello(){
+        return carrierRepository.findAll();;
+    }
+}
